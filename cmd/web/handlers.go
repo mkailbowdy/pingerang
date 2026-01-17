37e3dee1753fc56e5cc3dbbbddbacbd0d62265e4
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2026-01-17 23:51:07 +0000

    Feature: HTMX

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,15 +3,92 @@ package main
 import (
 	"errors"
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/mkailbowdy/internal/models"
 )
 
+type Contact struct {
+	FirstName string
+	LastName  string
+	Email     string
+}
+
+var contact = Contact{
+	FirstName: "Gorge",
+	LastName:  "Fart",
+	Email:     "email@gmpa.com",
+}
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	w.Write([]byte("Hello from Pingerang"))
+
+	// Include the navigation partial in the template files.
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/partials/form.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", contact)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
+
+func (app *application) contact(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+		"./ui/html/partials/form.tmpl.html",
+		"./ui/html/pages/contact.tmpl.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	ts.ExecuteTemplate(w, "base", contact)
+}
+
+func (app *application) viewForm(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/html/partials/form.tmpl.html",
+	}
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	ts.ExecuteTemplate(w, "contact-view", contact)
+}
+
+func (app *application) editForm(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/html/partials/form.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	ts.ExecuteTemplate(w, "contact-edit", contact)
 }
 
 func (app *application) urlCreatePost(w http.ResponseWriter, r *http.Request) {
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,7 +6,12 @@ import (
 
 func (app *application) routes() *http.ServeMux {
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /contact/1", app.contact)
+	mux.HandleFunc("GET /contact/1/view", app.viewForm)
+	mux.HandleFunc("GET /contact/1/edit", app.editForm)
 	mux.HandleFunc("POST /url", app.urlCreatePost)
 	mux.HandleFunc("POST /url/compare", app.urlComparePost)
 
